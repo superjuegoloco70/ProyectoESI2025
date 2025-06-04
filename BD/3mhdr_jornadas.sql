7bf2a1e566c156e20b8df2b47622da15ab531c98
superjuegoloco70 <141503162+superjuegoloco70@users.noreply.github.com>
2025-06-04 16:21:18 -0300

    Added unpopulated Database

---
+++ BD/3mhdr_jornadas.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: 3mhdr
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.27-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `jornadas`
+--
+
+DROP TABLE IF EXISTS `jornadas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jornadas` (
+  `ID_Jornada` int(11) NOT NULL AUTO_INCREMENT,
+  `Fecha` date DEFAULT NULL,
+  `ID_ConstruccionTrabajada` int(11) DEFAULT NULL,
+  `CI_Jornalero` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Jornada`),
+  KEY `CI_Jornalero` (`CI_Jornalero`),
+  KEY `ID_ConstruccionTrabajada` (`ID_ConstruccionTrabajada`),
+  CONSTRAINT `jornadas_ibfk_1` FOREIGN KEY (`CI_Jornalero`) REFERENCES `usuarios` (`CI`),
+  CONSTRAINT `jornadas_ibfk_2` FOREIGN KEY (`ID_ConstruccionTrabajada`) REFERENCES `construcciones` (`ID_Construccion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jornadas`
+--
+
+LOCK TABLES `jornadas` WRITE;
+/*!40000 ALTER TABLE `jornadas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jornadas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-04 16:20:34
