bd032f7065438a6420782d0d859c397a641ff3e6
superjuegoloco70 <141503162+superjuegoloco70@users.noreply.github.com>
2025-06-11 11:27:47 -0300

    Archivos actualizados
    Falta reemplazar registro_api por apiUsuarios en todos los demás archivos que lo utilizen

--- apiUsuarios.php
+++ apiUsuarios.php
@@ -5,40 +5,45 @@ header("Content-Type: application/json");
 $method = $_SERVER["REQUEST_METHOD"];
 $input = json_decode(file_get_contents("php://input"), true);
 
+
+$con = new db();
+
 switch ($method){
     case "GET":
         if(isset($_GET["id"])){
             $id = $_GET["id"];
-            $result = $conn->query("Select * from usuarios where CI=$id");
-            $date = $result->fetch_assoc();
+            $query = "Select * from usuarios where CI=$id";
+            $result = $con->query($query);
+            $data = $result->fetch_assoc();
             echo json_encode($data);
         }else{
-            $result = $conn->query("Select * from usuarios");
+            $result = $con->query("Select * from usuarios");
             $users = [];
             while ($row = $result->fetch_assoc()){
                 $users[] = $row;
-            }
+            } 
             echo json_encode($users);
         }
         break;
      case 'POST':
-        $name = $input['name'];
-        $id = $input['id'];
-        $passwd = $input['passwd'];
-        $conn->query("INSERT INTO usuarios (CI, Nombre, Contraseña) VALUES ('$id', '$name', $passwd)");
+        $name = $_GET['name'];
+        $id = $_GET['id'];
+        $passwd = $_GET['passwd'];
+        $query = "INSERT INTO usuarios (CI, Nombre, Contraseña) VALUES ('$id', '$name', '$passwd')";
+        $con->query($query);
         echo json_encode(["message" => "User added successfully"]);
         break;
 
     case 'PUT':
         $id = $_GET['id'];
-        $socio = $input['socio'];
-        $conn->query("UPDATE usuarios SET EsSocio='$socio', WHERE CI=$id");
+        $socio = $_GET['socio'];
+        $con->query("UPDATE usuarios SET EsSocio='$socio' WHERE CI=$id");
         echo json_encode(["message" => "User updated successfully"]);
         break;
 
     case 'DELETE':
         $id = $_GET['id'];
-        $conn->query("DELETE FROM usuarios WHERE CI=$id");
+        $con->query("DELETE FROM usuarios WHERE CI=$id");
         echo json_encode(["message" => "User deleted successfully"]);
         break;
 
@@ -47,7 +52,7 @@ switch ($method){
         break;
 }
 
-$conn->close();
+
 
 
 
