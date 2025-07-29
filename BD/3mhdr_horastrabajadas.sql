bd28d38aec118451f8b60a95fe8eebce4dc30d2a
superjuegoloco70 <141503162+superjuegoloco70@users.noreply.github.com>
2025-07-29 18:44:36 -0300

    Actualizado Base de datos

--- BD/3mhdr_horastrabajadas.sql
+++ BD/3mhdr_horastrabajadas.sql
@@ -16,33 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `usuarios`
+-- Table structure for table `horastrabajadas`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
+DROP TABLE IF EXISTS `horastrabajadas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `CI` int(11) NOT NULL,
-  `Nombre` varchar(255) NOT NULL,
-  `Contrasena` varchar(255) NOT NULL,
-  `Sueldo` varchar(255) DEFAULT NULL,
-  `ID_ViviendaUsuario` int(11) DEFAULT NULL,
-  `Estado` int(11) DEFAULT NULL,
-  PRIMARY KEY (`CI`),
-  KEY `ID_ViviendaUsuario` (`ID_ViviendaUsuario`),
-  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`ID_ViviendaUsuario`) REFERENCES `vivienda` (`ID_Vivienda`)
+CREATE TABLE `horastrabajadas` (
+  `id_horastrabajadas` int(11) NOT NULL AUTO_INCREMENT,
+  `FechaTrabajo` date NOT NULL,
+  `N_Horas` int(11) NOT NULL,
+  `CI_Trabajador` int(11) NOT NULL,
+  PRIMARY KEY (`id_horastrabajadas`),
+  KEY `CI_Trabajador` (`CI_Trabajador`),
+  CONSTRAINT `horastrabajadas_ibfk_1` FOREIGN KEY (`CI_Trabajador`) REFERENCES `usuarios` (`CI`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuarios`
+-- Dumping data for table `horastrabajadas`
 --
 
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (81,'lala','898','',NULL,0),(85,'sadsad','2131','',NULL,NULL),(89,'Pepe Silva','567','',NULL,NULL),(589,'Koko','hola123','',NULL,NULL),(999,'Aleman','pepito45',NULL,NULL,1),(5968954,'fsdfds','opopdas',NULL,NULL,0),(11111111,'Carlita','54321','',NULL,NULL),(56984963,'P4Aloss','221220071',NULL,NULL,0);
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+LOCK TABLES `horastrabajadas` WRITE;
+/*!40000 ALTER TABLE `horastrabajadas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `horastrabajadas` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
