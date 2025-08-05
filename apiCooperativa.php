2b7458e5495ae4dcb24996c96cb0c4e4982046fd
superjuegoloco70 <141503162+superjuegoloco70@users.noreply.github.com>
2025-08-05 12:25:18 -0300

    Actualizado archivos
    Actualizado conexion.php, registrarHoras.html y apiCooperativa.php
    registrarHoras.html y apiCooperativa.php ahora usa JSON

--- apiCooperativa.php
+++ apiCooperativa.php
@@ -1,21 +1,28 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include "conexion.php";
 
 $con = new db();
 
 header("Content-Type: application/json");
+$input= json_decode(file_get_contents("php://input"), true);
 $method = $_SERVER["REQUEST_METHOD"];
-$input = json_decode(file_get_contents("php://input"), true);
+
+
 
 switch ($method){
-    case "PUT":
-        if(isset($_POST["fecha"]) && isset($_POST["horas"])){
-            $con->registerHours($_POST["fecha"], $_POST["horas"]);
+    case "POST":
+        if($input["accion"] == "registrar"){
+            $con->registerHours($input["fecha"], $input["horas"]);
             echo json_encode(["message" => "Horas Registradas Correctamente"]);
         }else{
             echo json_encode(["message" => "Error en el registro de horas"]);
         }
+        break;
     default:
         echo json_encode(["message" => "Invalid request method"]);
         break;
