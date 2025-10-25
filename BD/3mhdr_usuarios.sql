fae0338f251364e200676f8e9a4a0b8047756436
superjuegoloco70 <141503162+superjuegoloco70@users.noreply.github.com>
2025-10-25 16:02:43 -0300

    Limpiado base de datos

--- BD/3mhdr_usuarios.sql
+++ BD/3mhdr_usuarios.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
--- Host: localhost    Database: 3mhdr
+-- Host: 127.0.0.1    Database: 3mhdr
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.27-MariaDB
+-- Server version	5.5.5-10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,11 +27,8 @@ CREATE TABLE `usuarios` (
   `Nombre` varchar(255) NOT NULL,
   `Contrasena` varchar(255) NOT NULL,
   `Sueldo` varchar(255) DEFAULT NULL,
-  `ID_ViviendaUsuario` int(11) DEFAULT NULL,
   `Estado` int(11) DEFAULT NULL,
-  PRIMARY KEY (`CI`),
-  KEY `ID_ViviendaUsuario` (`ID_ViviendaUsuario`),
-  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`ID_ViviendaUsuario`) REFERENCES `vivienda` (`ID_Vivienda`)
+  PRIMARY KEY (`CI`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -41,7 +38,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (81,'lala','898','',NULL,0),(85,'sadsad','2131','',NULL,NULL),(89,'Pepe Silva','567','',NULL,NULL),(589,'Koko','hola123','',NULL,NULL),(789,'OOO','qwerty',NULL,NULL,0),(999,'Aleman','pepito45',NULL,NULL,1),(5668,'1234','9090',NULL,NULL,0),(12313,'Hola','jhiaopj',NULL,NULL,0),(999888,'Hola','pololo',NULL,NULL,0),(5968954,'fsdfds','opopdas',NULL,NULL,0),(9998887,'Hola','holita',NULL,NULL,0),(11111111,'Carlita','54321','',NULL,NULL),(56984963,'P4Aloss','221220071',NULL,NULL,1);
+INSERT INTO `usuarios` VALUES (81634623,'lala','898','',2),(87452812,'DR','pi',NULL,0),(98456342,'Aleman','pepito45',NULL,1);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-27 17:52:31
+-- Dump completed on 2025-10-25 16:00:25
